991550ac4e42852a6b9edafb31d0b2e43bab5e77
DC <fma727@hotmail.com>
2019-12-01 19:31:14 -0500

    adding test for BeforeTurn

---
+++ app/components/Companion18cz/BeforeTurn/BeforeTurn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// eslint-disable-next-line arrow-body-style
+const BeforeTurn = () => {
+  return <div></div>;
+};
+
+export default BeforeTurn;
--- app/containers/Companion18cz/tests/index.test.js
+++ app/containers/Companion18cz/tests/index.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import BeforeTurn from 'components/Companion18cz/BeforeTurn';
 import Companion18cz from '../index';
 
 describe('<18cz />', () => {
   it('should render title', () => {
     const renderedComponent = shallow(<Companion18cz />);
     expect(renderedComponent.find('h1').length).toBe(1);
+  });
+
+  it('should render BeforeTurn', () => {
+    const renderedComponent = shallow(<Companion18cz />);
+    expect(renderedComponent.find(BeforeTurn).length).toBe(1);
   });
 });
