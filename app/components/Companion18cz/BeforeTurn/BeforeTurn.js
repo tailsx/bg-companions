a2d8e682e0c2e5d505164e29ec2b83cc84c5edfc
DC <fma727@hotmail.com>
2019-12-02 01:37:52 -0500

    passing BeforeTurn test

--- app/components/Companion18cz/BeforeTurn/BeforeTurn.js
+++ app/components/Companion18cz/BeforeTurn/BeforeTurn.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 // eslint-disable-next-line arrow-body-style
 const BeforeTurn = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Before Turn</h1>
+    </div>
+  );
 };
 
 export default BeforeTurn;
