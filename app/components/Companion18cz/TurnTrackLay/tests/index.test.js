566833edeb44753f7a1ae6c849038c7e22d853c6
DC <fma727@hotmail.com>
2019-12-05 13:15:35 -0500

    adding test for TurnTrackLay

---
+++ app/components/Companion18cz/TurnTrackLay/tests/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Train from '../../Train';
+import TurnTrackLay from '../index';
+
+describe('<TurnTrackLay>', () => {
+  const trains = [
+    { type: 'train', name: 'test-train', lastRan: 100 },
+    { type: 'train', name: 'test-train', lastRan: 100 },
+  ];
+  it('should render components', () => {
+    const component = shallow(<TurnTrackLay trains={trains} />);
+
+    expect(component.find(Train)).toBe(1);
+  });
+});
--- app/containers/Companion18cz/tests/index.test.js
+++ app/containers/Companion18cz/tests/index.test.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import BeforeTurn from 'components/Companion18cz/BeforeTurn';
+import TurnTrackLay from 'components/Companion18cz/TurnTrackLay';
 import Companion18cz from '../Companion18cz';
 // import { mapDispatchToProps } from '../index';
 
 describe('<18cz />', () => {
   it('should render title', () => {
     const renderedComponent = shallow(<Companion18cz />);
-    expect(renderedComponent.find('h1').length).toBe(1);
+    expect(renderedComponent.find('h1')).toHaveLength(1);
   });
 
-  it('should render BeforeTurn', () => {
-    const renderedComponent = shallow(<Companion18cz />);
-    expect(renderedComponent.find(BeforeTurn).length).toBe(1);
+  it('should render components', () => {
+    const component = shallow(<Companion18cz />);
+
+    expect(component.find(BeforeTurn)).toHaveLength(1);
+    expect(component.find(TurnTrackLay)).toHaveLength(1);
   });
 });
