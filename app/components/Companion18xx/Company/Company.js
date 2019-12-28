133b178f3764d365bb5d6e2fdb6ddabf9f7f77c4
DC <fma727@hotmail.com>
2019-12-27 23:20:50 -0500

    adding some props

--- app/components/Companion18xx/Company/Company.js
+++ app/components/Companion18xx/Company/Company.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Company = ({ id, colour }) => <div>{`Company: ${id}, Color: ${colour}`}</div>;
+const Company = ({ id, colour, onFloatCompany, onChangeFloatValue }) => (
+  <div>
+    {`Company: ${id}, Color: ${colour}`}
+    <input type="number" onChange={onChangeFloatValue} />
+    <button onClick={onFloatCompany}>float Company</button>
+    <div>Childen stuff</div>
+  </div>
+);
 
 export default Company;
