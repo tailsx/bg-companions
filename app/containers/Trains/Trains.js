7ec21b02a9d94ebc61d4b27e6e23c09bab56265e
DC <fma727@hotmail.com>
2020-01-01 23:14:17 -0500

    removing flexs

--- app/containers/Trains/Trains.js
+++ app/containers/Trains/Trains.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Train from 'containers/Train';
 
+import './style.scss';
+
 const Trains = ({ trainIds, onCreateTrain, allTrainsRevenue }) => {
   return (
     <div>
