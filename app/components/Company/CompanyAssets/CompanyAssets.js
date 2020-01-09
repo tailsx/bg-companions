d3eb5b9a3e9bc60d892127c93f2b6647b8e3868c
DC <fma727@hotmail.com>
2020-01-09 14:35:52 -0800

    adding treasury

--- app/components/Company/CompanyAssets/CompanyAssets.js
+++ app/components/Company/CompanyAssets/CompanyAssets.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Treasury from 'components/Treasury';
 import Trains from 'containers/Trains';
 
 import './style.scss';
-import Treasury from 'components/Treasury';
 
 const CompanyAssets = ({ companyId, trainIds, treasury }) => (
   <div className="company-assets">
-    <div className="company-assets__text">
-      {`Comapny ID: ${companyId}`}
+    <div className="company-assets__section">
       <Treasury companyId={companyId} treasury={treasury} />
     </div>
-
-    <div className="company-assets__trains">
-      <span>Trains</span>
-      <Trains companyId={companyId} trainIds={trainIds} />
+    <div className="company-assets__section">
+      <div className="company-assets__section company-assets__trains">
+        <span>Trains</span>
+        <Trains companyId={companyId} trainIds={trainIds} />
+      </div>
     </div>
   </div>
 );
