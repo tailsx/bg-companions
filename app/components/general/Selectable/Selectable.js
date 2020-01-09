397d586d93d4995d0c860cd7bce20b6c38c47028
DC <fma727@hotmail.com>
2020-01-09 12:44:50 -0800

    styling selectable

--- app/components/general/Selectable/Selectable.js
+++ app/components/general/Selectable/Selectable.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './style.scss';
+
 const VIEWABLE_OPTIONS = 1;
 
 const Selectable = ({ onValueChange, onButtonClick, options, buttonText }) => (
-  <div>
-    <select size={VIEWABLE_OPTIONS} onChange={onValueChange}>
+  <div className="selectable">
+    <select className="selectable__dropdown" size={VIEWABLE_OPTIONS} onChange={onValueChange}>
       {options.map(item => (
         <option key={`select-${item}`} value={item}>
           {item}
         </option>
       ))}
     </select>
-    <button type="button" onClick={onButtonClick}>
+    <button className="selectable__button" type="button" onClick={onButtonClick}>
       {buttonText}
     </button>
   </div>
