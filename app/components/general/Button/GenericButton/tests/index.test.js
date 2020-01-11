cca6d89b8423f3c62b7ccfc15a34e723265e7444
DC <fma727@hotmail.com>
2020-01-10 19:06:02 -0800

    fixing button at top

---
+++ app/components/general/Button/GenericButton/tests/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import GenericButton from '../index';
+
+describe('<Header />', () => {
+  it('should render a div', () => {
+    const renderedComponent = shallow(<GenericButton />);
+    expect(renderedComponent.length).toEqual(1);
+  });
+
+  it('should have a .btn class', () => {
+  })
+});
